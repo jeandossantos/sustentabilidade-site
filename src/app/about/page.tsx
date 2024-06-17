78bf9278c1e5e40897b180a80cab7fe4815156fa
Jean F. dos  Santos <jeanddg@hotmail.com>
2024-06-17 17:58:45 -0300

    feat: improve about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,8 +2,8 @@ export default function About() {
   return (
     <>
       <h2 className='text-4xl font-bold text-green-900'>Sobre o Site</h2>
-      <p>
-        <b className='text-zinc-800'>O +Sustentabilidade</b> foi criado para
+      <p className='mt-3'>
+        <b className='text-zinc-800 '>O +Sustentabilidade</b> foi criado para
         promover a conscientização sobre os 5 R&apos;s da sustentabilidade:{' '}
         <b className='text-zic-800'>
           Reduzir, Reutilizar, Reciclar, Recusar e Repensar
@@ -12,11 +12,16 @@ export default function About() {
         sustentável.
       </p>
 
-      <h2 className='text-3xl mt-5 font-bold text-green-700'>Sobre Mim</h2>
-      <p>
-        Meu nome é Jean F. Santos e sou estudante de Análise e Desenvolvimento
-        de Sistemas (ADS) na INIG (Universidade de Nova Iguaçu). Estou no
-        segundo período do curso, que é oferecido na modalidade EAD.
+      <p className='mt-4'>
+        O <b className='text-zinc-800'>+Sustentabilidade</b> é parte do projeto
+        integrador da disciplina EcoAprendizado:{' '}
+        <b className='text-zinc-800 '>
+          Integrando Sustentabilidade e Tecnologia
+        </b>{' '}
+        do curso de Análise e Desenvolvimento de Sistemas (ADS) da{' '}
+        <b className='text-zinc-800 '>UNIG</b>. Através deste projeto, os alunos
+        têm a oportunidade de colocar em prática seus conhecimentos e
+        habilidades para contribuir para um futuro mais verde.
       </p>
     </>
   );
